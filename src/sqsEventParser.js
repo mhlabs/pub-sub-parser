445aaf0a6d9b04f81908161487e0c31c0c95e8e9
Jens-Peter Olsson <jens-peter.olsson@it-budet.se>
2020-12-18 12:57:29 +0100

    chore: better record verification and tests

--- src/sqsEventParser.js
+++ src/sqsEventParser.js
@@ -6,20 +6,30 @@ const pubSubKey = 'PubSub_S3Key';
 const s3 = new AWS.S3();
 
 function shouldDownload(eventRecord) {
-  return eventRecord && eventRecord.body && eventRecord.body === '#';
+  return (
+    eventRecord &&
+    eventRecord.body &&
+    eventRecord.body === '#' &&
+    eventRecord.messageAttributes &&
+    eventRecord.messageAttributes[pubSubBucket]
+  );
+}
+
+function getS3Params(eventRecord) {
+  const bucket = eventRecord.messageAttributes[pubSubBucket].stringValue;
+  const key = eventRecord.messageAttributes[pubSubKey].stringValue;
+
+  return { Bucket: bucket, Key: key };
 }
 
 async function parse(eventRecord) {
   if (!shouldDownload(eventRecord)) return Promise.resolve(eventRecord.body);
 
-  if (!eventRecord.MessageAttributes[pubSubBucket]) {
+  if (!eventRecord.messageAttributes[pubSubBucket]) {
     return Promise.resolve(eventRecord.body);
   }
 
-  const bucket = eventRecord.messageAttributes[pubSubBucket].stringValue;
-  const key = eventRecord.messageAttributes[pubSubKey].stringValue;
-
-  const params = { Bucket: bucket, Key: key };
+  const params = getS3Params();
 
   try {
     const data = await s3.getObject(params).promise();
@@ -31,6 +41,7 @@ async function parse(eventRecord) {
 }
 
 module.exports = {
+  getS3Params,
   parse,
   shouldDownload
 };
--- src/sqsEventParser.test.js
+++ src/sqsEventParser.test.js
@@ -1,10 +1,20 @@
 const tested = require('./sqsEventParser');
 
+const testRecord = require('../testData/sqsRecord');
+
 describe('download', () => {
   it('should download large messages', () => {
-    expect(tested.shouldDownload({ body: '#' })).toBeTruthy();
+    expect(tested.shouldDownload(testRecord)).toBeTruthy();
   });
   it('should not download inline messages', () => {
     expect(tested.shouldDownload({ body: '"{"id": 1}"' })).toBeFalsy();
+  });
+});
+
+describe('s3 params', () => {
+  it('should parse params from event record', () => {
+    const params = tested.getS3Params(testRecord);
+    expect(params.Bucket).toBe('s3-bucket');
+    expect(params.Key).toBe('s3-key');
   });
 });
